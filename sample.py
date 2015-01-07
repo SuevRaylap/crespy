7a75ec243cccffd422ae84abe3a9c3c8937e7eba
SuevRaylap <suev.raylap@gmail.com>
2015-01-06 22:33:29 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,19 +1,31 @@
 from crespy import get_crest_root
 
-# this should be changed to uniquely identify your application
 root = get_crest_root('crespy-sample')
 
-print root
+print(root)
 
 specs = root.industry.specialities.href
 specs.load()
-#print specs
+
+print(specs)
+
+regions = root.regions.href
+regions.load()
+
+print(regions)
+
+for items in regions['items']:
+#  #print items.name
+  if items.name == 'B-R00004':
+    print("it worked")
+    print(items.name)
 
 prices = root.marketPrices.href
 prices.load()
 
+print(prices)
+
 # you need to do it this way because items is a method on the collection class
 for item in prices['items']:
   if item.type.id == 29668:
-    print "The price of PLEX is too damn high: {:,} ISK".format(item.averagePrice)
-
+    print("The price of PLEX is too damn high: {:,} ISK".format(item.averagePrice))
